ba46d15416cedc4b512f26d50ae40c4cafaf9d6c
richard <ricstewa41@cox.net>
2024-02-23 13:48:53 -0600

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import {BrowserRouter} from 'react-router-dom'
+import {Routes, Route, Link} from 'react-router-dom'
+import Red from './components/Red'
+import Blue from './components/Blue'
+import Green from './components/Green'
+import Purple from './components/Purple'
+import Yellow from './components/Yellow'
 import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div id="navbar">
+          <Link className="home" to="/home">Home</Link>
+          <Link className="yellow" to="/yellow">YELLOW</Link>
+          <Link className="red" to="/red" >RED</Link>
+          <Link className="green" to="/green">GREEN</Link>
+          <Link className="blue" to="/blue" >BLUE</Link>
+          <Link className="purple" to="/purple" >PURPLE</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/yellow" element={<h1>Yellow</h1>} />
+          <Route path="/blue" element={<h1>Blue</h1>} />
+          <Route path="/purple" element={<h1>Purple</h1>} />
+          <Route path="/green" element={<h1>Green</h1>} />
+          <Route path="/red" element={<h1>Red</h1>} />
+          </Routes>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/Yellow.jsx
@@ -0,0 +1,10 @@
+function Yellow() {
+    return (
+       
+            <div className="yellow">
+            <h1>Yellow</h1>
+        </div>
+       
+    )
+}
+export default Yellow
